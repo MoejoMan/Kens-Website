9e7a544d78a4ecb537a1ddc44acb3b2044399319
jhg56 <jhg567999@gmail.com>
2025-10-27 22:17:47 +0000

    fixed app py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -14,7 +15,6 @@ def careers():
 def contact():
     return render_template('contact.html')
 
-app.run(debug=True)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
